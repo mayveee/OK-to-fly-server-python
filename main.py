532b92e275c8ab9d35960adc561adec787f49800
mayvee <hjwoo0812@naver.com>
2025-05-08 10:13:01 +0900

    CORS 설정

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
 from routes import analysis
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 app.include_router(analysis.router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def root():
